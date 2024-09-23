44cb0522c0fd80a40dab926d813bd9353df7a725
VietTran8 <cubon882003@gmail.com>
2024-09-23 16:30:47 +0700

    init web client

--- server/user-service/src/main/java/vn/edu/tdtu/services/FileService.java
+++ server/user-service/src/main/java/vn/edu/tdtu/services/FileService.java
@@ -1,26 +1,13 @@
 package vn.edu.tdtu.services;
 
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import vn.edu.tdtu.dtos.ResDTO;
-import vn.edu.tdtu.dtos.request.FileReq;
 import vn.edu.tdtu.enums.EFileType;
 import vn.edu.tdtu.repositories.httpclient.FileClient;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.Map;
 
 @Service
